9da84fb26fdd5570330904d225cab610efb2385b
gauraklein <gaurakklein@gmail.com>
2019-12-13 01:31:43 -0600

    layout is looking a little cleaner, considering ditching bulma all together:

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 let typedTextContainer = document.getElementById('typedText')
+let possiblyCheesyIntro = [`<font color="blue">const</font> Introduction <font color="white">=</font> <font color="yellow">{</font><br>
+  greeting: <font color="#ba7d65">"Hello World!"</font><font color="white">,</font><br>
+  name: <font color="#ba7d65">"Gaura Klein"</font><font color="white">,</font><br>
+  occupation: <font color="#ba7d65">"Software Developer"</font><br>
+  <font color="yellow">}</font><font color="white">;</font>`]
+
+let basicIntro = ["Hi, I'm Gaura. <br> A Software Developer."]
 
 let sentence = {
-  strings: [`<font color="blue">const</font> Introduction <font color="white">=</font> <font color="yellow">{</font><br>
-    greeting: <font color="#ba7d65">"Hello World!"</font><font color="white">,</font><br>
-    name: <font color="#ba7d65">"Gaura Klein"</font><font color="white">,</font><br>
-    occupation: <font color="#ba7d65">"Software Developer"</font><br>
-    <font color="yellow">}</font><font color="white">;</font>`],
+  strings: basicIntro,
   typeSpeed: 30,
   showCursor: false
 };
